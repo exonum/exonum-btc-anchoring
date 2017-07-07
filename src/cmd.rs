c73c69463881714b70cd10f97c05467acfac94c2
Vladimir Motylenko <vladimir.motylenko@xdev.re>
2017-07-07 11:20:37 +0300

    Update implementation

--- src/cmd.rs
+++ src/cmd.rs
@@ -16,7 +16,8 @@ use std::collections::BTreeMap;
 
 use service::AnchoringService;
 use super::{AnchoringConfig, AnchoringNodeConfig, AnchoringRpcConfig, gen_btc_keypair};
-use exonum::helpers::clap::{ConfigTemplate };
+use exonum::helpers::fabric::{NodePublicConfig, AbstractConfig,
+                                CommonConfigTemplate};
 
 use details::btc::{ PublicKey, PrivateKey};
 
@@ -33,55 +34,81 @@ pub struct AnchoringServiceConfig {
     pub node: AnchoringNodeConfig,
 }
 
-struct KeygenCommand;
+struct GenerateNodeConfig;
 
-impl CommandExtension for KeygenCommand {
+impl CommandExtension for GenerateNodeConfig {
     fn args(&self) -> Vec<Argument> {
         vec![
-            Argument::new_positional("NETWORK", true,
-            "Anchoring network name."),
+            Argument::new_named("ANCHORING_RPC_HOST", true,
+            "Host of bitcoind.", None, "anchoring-host", false),
+            Argument::new_named("ANCHORING_RPC_USER", false,
+            "User to login into bitcoind.",  None, "anchoring-user", false),
+            Argument::new_named("ANCHORING_RPC_PASSWD", false,
+            "Password to login into bitcoind.",  None, "anchoring-password", false),
         ]
     }
 
     fn execute(&self, mut context: Context) -> Result<Context, Box<Error>> {
-        let network = match context.arg::<String>("NETWORK")
-                                   .expect("No network name found.").as_str() {
+        let host = context.arg("ANCHORING_RPC_HOST")
+                            .expect("Expected ANCHORING_RPC_HOST");
+        let user = context.arg("ANCHORING_RPC_USER").ok();
+        let passwd = context.arg("ANCHORING_RPC_PASSWD").ok();
+
+        let config: CommonConfigTemplate = context.get("common_config").unwrap();
+
+        let network: String = config.services_config.get("anchoring_network")
+                                            .expect("No network name found.")
+                                            .clone()
+                                            .try_into()
+                                            .unwrap();
+        let network = match network.as_str() {
                 "testnet" => Network::Testnet,
                 "bitcoin" => Network::Bitcoin,
                 _ => panic!("Wrong network type"),
         };
+
         let (p, s) = gen_btc_keypair(network);
-        let mut services_pub_keys: BTreeMap<String, Value> = 
-            context.get("services_pub_keys")
+        let mut services_public_configs: BTreeMap<String, Value> = 
+            context.get("services_public_configs")
                 .unwrap_or_default();
-        services_pub_keys.extend(vec![("anchoring_pub_key".to_owned(), 
+        services_public_configs.extend(vec![("anchoring_pub_key".to_owned(), 
                             Value::try_from(p.to_hex()).unwrap())].into_iter());
-
-        let mut services_sec_keys: BTreeMap<String, Value> = 
-            context.get("services_sec_keys")
+        
+        let rpc_config = AnchoringRpcConfig {
+                host: host,
+                username: user,
+                password: passwd,
+            };
+        //TODO: Replace this by structure.
+        let mut services_secret_configs: BTreeMap<String, Value> = 
+            context.get("services_secret_configs")
                 .unwrap_or_default();
-        services_sec_keys.extend(vec![("anchoring_sec_key".to_owned(), 
-                            Value::try_from(s.to_base58check()).unwrap()),
-                        ("anchoring_pub_key".to_owned(), 
-                            Value::try_from(p.to_hex()).unwrap())].into_iter());
+        services_secret_configs.extend(
+            vec![
+            ("anchoring_sec_key".to_owned(), 
+                Value::try_from(s.to_base58check()).unwrap()),
+            ("anchoring_pub_key".to_owned(), 
+                Value::try_from(p.to_hex()).unwrap()),
+            ("rpc_config".to_owned(), Value::try_from(rpc_config).unwrap())
+            ].into_iter());
 
-        context.set("services_pub_keys", services_pub_keys);
-        context.set("services_sec_keys", services_sec_keys);
+        context.set("services_public_configs", services_public_configs);
+        context.set("services_secret_configs", services_secret_configs);
         Ok(context)
     }
 }
 
-struct GenerateTemplateCommand;
+struct GenerateCommonConfig;
 
-impl CommandExtension for GenerateTemplateCommand {
+impl CommandExtension for GenerateCommonConfig {
     fn args(&self) -> Vec<Argument> {
         vec![
             Argument::new_named("ANCHORING_FREQUENCY", false,
-            "The frequency of anchoring in blocks", None, "anchoring-frequency"),
+            "The frequency of anchoring in blocks", None, "anchoring-frequency", false),
             Argument::new_named("ANCHORING_UTXO_CONFIRMATIONS", false,
-            "The minimum number of confirmations for anchoring transactions", None, "anchoring-utxo-confirmations"),
+            "The minimum number of confirmations for anchoring transactions", None, "anchoring-utxo-confirmations", false),
             Argument::new_named("ANCHORING_FEE", true,
-            "Fee that anchoring nodes should use.",  None, "anchoring-fee"),
+            "Fee that anchoring nodes should use.",  None, "anchoring-fee", false),
             Argument::new_positional("NETWORK", true,
             "Anchoring network name."),
         ]
@@ -98,87 +125,93 @@ impl CommandExtension for GenerateTemplateCommand {
                                    .expect("No network name found.");
             
             let mut values: BTreeMap<String, Value> = 
-                context.get("VALUE")
-                    .expect("Expected VALUES in context.");
+                context.get("services_config")
+                    .expect("Expected services_config in context.");
 
             values.extend(
-                        vec![("ANCHORING_FREQUENCY".to_owned(),
+                        vec![
+                            ("anchoring_frequency".to_owned(),
                                 Value::try_from(anchoring_frequency).unwrap()),
-                            ("ANCHORING_UTXO_CONFIRMATIONS".to_owned(),
+                            ("anchoring_utxo_confirmations".to_owned(),
                                 Value::try_from(anchoring_utxo_confirmations).unwrap()),
-                            ("ANCHORING_FEE".to_owned(),
+                            ("anchoring_fee".to_owned(),
                                 Value::try_from(fee).unwrap()),
-                            ("ANCHORING_NETWORK".to_owned(),
+                            ("anchoring_network".to_owned(),
                                 Value::try_from(network).unwrap())]
                         .into_iter());
-            context.set("VALUE", values);
+            context.set("services_config", values);
             Ok(context)
     }
 }
 
-struct InitCommand;
+struct Finalize;
 
-impl CommandExtension for InitCommand {
+impl CommandExtension for Finalize {
     fn args(&self) -> Vec<Argument> {
         vec![
-            Argument::new_named("ANCHORING_RPC_HOST", true,
-            "Host of bitcoind.", None, "anchoring-host"),
-            Argument::new_named("ANCHORING_RPC_USER", false,
-            "User to login into bitcoind.",  None, "anchoring-user"),
-            Argument::new_named("ANCHORING_RPC_PASSWD", false,
-            "Password to login into bitcoind.",  None, "anchoring-password"),
             Argument::new_named("ANCHORING_FUNDING_TXID", false, 
-            "Txid of the initial funding tx", None, "anchoring-funding-txid"),
+            "Txid of the initial funding tx", None, "anchoring-funding-txid", false),
             Argument::new_named("ANCHORING_CREATE_FUNDING_TX", false, 
-            "Create initial funding tx with given amount in satoshis", None, "anchoring-create-funding-tx")
-        ]
+            "Create initial funding tx with given amount in satoshis",
+            None, "anchoring-create-funding-tx", false)]
     }
 
     fn execute(&self, mut context: Context) -> Result<Context, Box<Error>> {
-            let host = context.arg("ANCHORING_RPC_HOST")
-                              .expect("Expected ANCHORING_RPC_HOST");
-            let user = context.arg("ANCHORING_RPC_USER").ok();
-            let passwd = context.arg("ANCHORING_RPC_PASSWD").ok();
-            let funding_txid = context.arg::<String>("ANCHORING_FUNDING_TXID").ok();
-            let create_funding_tx_with_amount = context.arg::<u64>("ANCHORING_CREATE_FUNDING_TX").ok();
-
             let mut node_config: NodeConfig = context.get("node_config").unwrap();
-            let template: ConfigTemplate = context.get("template").unwrap();
-            let keys: BTreeMap<String, Value> = context.get("services_sec_keys").unwrap();
+            let common_config: CommonConfigTemplate
+                                = context.get("common_config").unwrap();
+            let public_config_list: Vec<NodePublicConfig>
+                                = context.get("public_config_list").unwrap();
+            let services_secret_configs: AbstractConfig
+                                = context.get("services_secret_configs").unwrap();
 
-            let sec_key: String = keys.get("anchoring_sec_key")
+            let funding_txid = context.arg::<String>("ANCHORING_FUNDING_TXID").ok();
+            let create_funding_tx_with_amount =
+                            context.arg::<u64>("ANCHORING_CREATE_FUNDING_TX").ok();
+            let sec_key: String = services_secret_configs
+                            .get("anchoring_sec_key")
                             .expect("Anchoring secret key not found")
                             .clone()
                             .try_into()
                             .unwrap();
-            let pub_key: String = keys.get("anchoring_pub_key")
+            let pub_key: String = services_secret_configs
+                            .get("anchoring_pub_key")
                             .expect("Anchoring public key not fount")
                             .clone()
                             .try_into()
                             .unwrap();
-
-            let network: String = template.services.get("ANCHORING_NETWORK")
+            let rpc: AnchoringRpcConfig = services_secret_configs
+                            .get("rpc_config")
+                            .expect("Anchoring rpc config not fount")
+                            .clone()
+                            .try_into()
+                            .unwrap();
+            let network: String = common_config.services_config
+                            .get("anchoring_network")
                             .expect("Anchoring network not fount")
                             .clone()
                             .try_into()
                             .unwrap();
-            let utxo_confirmations: u64 = template.services.get("ANCHORING_UTXO_CONFIRMATIONS")
+            let utxo_confirmations: u64 = common_config.services_config
+                            .get("anchoring_utxo_confirmations")
                             .expect("Anchoring utxo confirmations not fount")
                             .clone()
                             .try_into()
                             .unwrap();
-            let frequency: u64 = template.services.get("ANCHORING_FREQUENCY")
+            let frequency: u64 = common_config.services_config
+                            .get("anchoring_frequency")
                             .expect("Anchoring frequency not fount")
                             .clone()
                             .try_into()
                             .unwrap();
-            let fee: u64 = template.services.get("ANCHORING_FEE")
+            let fee: u64 = common_config.services_config
+                            .get("anchoring_fee")
                             .expect("Anchoring fee not fount")
                             .clone()
                             .try_into()
                             .unwrap();
 
-            let network = match network.as_ref() {
+            let network = match network.as_str() {
                 "testnet" => Network::Testnet,
                 "bitcoin" => Network::Bitcoin,
                 _ => panic!("Wrong network type"),
@@ -187,31 +220,25 @@ impl CommandExtension for InitCommand {
             let priv_key: PrivateKey = PrivateKey::from_base58check(&sec_key).unwrap();
             //\TODO: validate config keys
             let _pub_key: PublicKey = HexValue::from_hex(&pub_key).unwrap();
-            let pub_keys: Vec<PublicKey> = template.validators()
+            let pub_keys: Vec<PublicKey> = public_config_list
                 .iter()
-                .map(|(_, ref v)|{
-                    let key: String = v.keys()
-                    .get("anchoring_pub_key")
-                    .expect("Anchoring validator public key not fount")
-                    .clone()
-                    .try_into()
-                    .unwrap();
+                .map(|v|{
+                    let key: String = v.services_public_configs()
+                        .get("anchoring_pub_key")
+                        .expect("Anchoring validator public key not fount")
+                        .clone()
+                        .try_into()
+                        .unwrap();
                     HexValue::from_hex(&key).unwrap()
                 }).collect();
-            let rpc = AnchoringRpcConfig {
-                host: host,
-                username: user,
-                password: passwd,
-            };
             let client = AnchoringRpc::new(rpc.clone());
             let mut anchoring_config = AnchoringNodeConfig::new(Some(rpc));
 
-            let majority_count = ::majority_count(template.count() as u8);
+            let majority_count = ::majority_count(public_config_list.len() as u8);
             let (_, address) = client
                     .create_multisig_address(network, majority_count, pub_keys.iter())
                     .unwrap();
             
-
             let mut genesis_cfg = if let Some(total_funds) = create_funding_tx_with_amount {
                 let tx = FundingTx::create(&client, &address, total_funds).unwrap();
                 println!("Created funding tx with txid {}", tx.txid());
@@ -248,9 +275,9 @@ impl ServiceFactory for AnchoringService {
     fn command(command: CommandName) -> Option<Box<CommandExtension>> {
         use exonum::helpers::fabric;
         Some(match command {
-            v if v == fabric::KeyGeneratorCommand::name() => Box::new(KeygenCommand),
-            v if v == fabric::GenerateTemplateCommand::name() => Box::new(GenerateTemplateCommand),
-            v if v == fabric::InitCommand::name() => Box::new(InitCommand),
+            v if v == fabric::GenerateNodeConfig::name() => Box::new(GenerateNodeConfig),
+            v if v == fabric::GenerateCommonConfig::name() => Box::new(GenerateCommonConfig),
+            v if v == fabric::Finalize::name() => Box::new(Finalize),
             _ => return None,
         })
     }
