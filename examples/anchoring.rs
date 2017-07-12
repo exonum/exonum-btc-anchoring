aaa92e3db60e8e2cbc19670b329149fd1f50857c
Vladimir Motylenko <vladimir.motylenko@xdev.re>
2017-07-12 14:54:29 +0300

    fix grammar, clippy errors

--- examples/anchoring.rs
+++ examples/anchoring.rs
@@ -1,14 +1,16 @@
 extern crate exonum;
 extern crate btc_anchoring_service;
-//extern crate configuration_service;
+extern crate configuration_service;
 use exonum::helpers::fabric::NodeBuilder;
 use exonum::helpers;
 use btc_anchoring_service::AnchoringService;
-//use configuration_service::ConfigurationService;
+use configuration_service::ConfigurationService;
 
 fn main() {
     exonum::crypto::init();
     helpers::init_logger().unwrap();
-    let node = NodeBuilder::new().with_service::<AnchoringService>();
+    let node = NodeBuilder::new()
+                    .with_service::<AnchoringService>()
+                    .with_service::<ConfigurationService>();
     node.run();
 }
