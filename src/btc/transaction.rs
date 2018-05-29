722adcb555019f451d3fee499b29367cde3f6366
Alexander Ponimaskin <ponimaskin@gmail.com>
2018-05-29 20:02:22 +0300

    some units

--- src/btc/transaction.rs
+++ src/btc/transaction.rs
@@ -138,7 +138,7 @@ impl BtcAnchoringTransactionBuilder {
                     prev_hash: tx.0.txid(),
                     prev_index: out_index as u32,
                     script_sig: Script::default(),
-                    sequence: 0xFFFFFFFF,
+                    sequence: 0xFFFF_FFFF,
                     witness: Vec::default(),
                 };
                 balance += tx.0.output[out_index].value;
@@ -169,6 +169,7 @@ impl BtcAnchoringTransactionBuilder {
                 },
             ],
         });
+
         // Computes a total fee value.
         let size_in_bytes = {
             let bytes = ::bitcoin::network::serialize::serialize(&transaction.0).unwrap();
@@ -186,15 +187,21 @@ impl BtcAnchoringTransactionBuilder {
 
 #[cfg(test)]
 mod tests {
+    use super::{BtcAnchoringTransactionBuilder, Transaction};
+    use bitcoin::blockdata::opcodes::All;
+    use bitcoin::blockdata::script::{Builder, Script};
+    use bitcoin::blockdata::transaction::{self, TxIn, TxOut};
+    use bitcoin::network::constants::Network;
+    use bitcoin::util::address::Address;
+    use bitcoin::util::hash::Sha256dHash;
+    use btc::PublicKey;
+    use btc_transaction_utils::multisig::RedeemScriptBuilder;
+    use exonum::crypto::CryptoHash;
     use exonum::crypto::Hash;
     use exonum::encoding::serialize::FromHex;
     use exonum::helpers::Height;
     use exonum::storage::StorageValue;
-
-    use bitcoin::network::constants::Network;
-    use bitcoin::util::address::Address;
-
-    use super::Transaction;
+    use std::borrow::Cow;
 
     #[test]
     fn test_transaction_conversions() {
@@ -258,28 +265,236 @@ mod tests {
         );
     }
 
-    #[test]
-    fn test_transaction_exonum_field() {
-        unimplemented!();
+    proptest! {
+        #[test]
+        fn test_transaction_exonum_field(input_num in 1usize..4,
+                                         prev_index in 1u32..10,
+                                         output_num in 1usize..4,
+                                         value in 1u64..1_000_000_000,
+                                         ref s in "\\PC*") {
+            let input = (0..input_num).map(|_| {
+                // just random hash
+                let prev_hash = Sha256dHash::from_data(s.as_bytes());
+                TxIn {
+                    prev_hash,
+                    prev_index,
+                    script_sig: Script::default(),
+                    sequence: 0xFFFFFFFF,
+                    witness: Vec::default(),
+                }
+            }).collect::<Vec<_>>();
+
+            let output = (0..output_num).map(|_| {
+                TxOut {
+                    value,
+                    script_pubkey: Builder::new()
+                        .push_opcode(All::OP_RETURN)
+                        .push_slice(s.as_bytes())
+                        .into_script(),
+                }
+            }).collect::<Vec<_>>();
+
+            let transaction = Transaction::from(transaction::Transaction {
+                version: 2,
+                lock_time: 0,
+                input,
+                output,
+            });
+
+            let bytes = transaction.clone().into_bytes();
+            assert_eq!(
+                transaction,
+                <Transaction as StorageValue>::from_bytes(Cow::Borrowed(&bytes))
+            );
+        }
     }
 
     #[test]
     fn test_anchoring_transaction_builder_simple() {
-        unimplemented!();
+        let funding_tx: Transaction = Transaction::from_hex(
+            "02000000000101b651818fe3855d0d5d74de1cf72b56503c16f808519440e842b6\
+             dc2dd570c4930100000000feffffff02deaa7b0000000000160014923904449829\
+             cd865cdfb72abdba0806ce9e48911027000000000000220020e9bb049fdff8f8d3\
+             b33b7335978b1dbb268833a32a69906f9e500e4103151bef02483045022100ddc7\
+             eb1193529a8d0e48cf24f536d5fbb5de3b67d2f56c98190ea8585d58a156022075\
+             e33981f1a7d78ce2915402d4b9b38b8d5311e0aef2e3ccf9284d2ce602968d0121\
+             021d0478acd223fb9b2ad7485f06f12914a1b7effc78390a08c50bfe53b3b24815\
+             062c1400",
+        ).unwrap();
+
+        let keys = vec![
+            "038b782f94d19f34536a96e12e0bad99e6f82c838fa16a4234572f5f132d95ba29",
+            "020ae2216f42575c4196864eda0252c75c61273065f691b32be9a99cb2a3c9b4d1",
+            "02536d5e1464b961562da57207e4a46edb7dade9b92aa29712ca8309c8aba5be5b",
+        ].iter()
+            .map(|h| PublicKey::from_hex(h).unwrap().0.clone())
+            .collect::<Vec<_>>();
+
+        let redeem_script = RedeemScriptBuilder::with_public_keys(keys)
+            .to_script()
+            .unwrap();
+
+        let (tx, inputs) = BtcAnchoringTransactionBuilder::new(redeem_script)
+            .additional_funds(funding_tx.clone())
+            .fee(1)
+            .payload(Height::zero(), funding_tx.hash())   // useless payload
+            .create()
+            .unwrap();
+
+        assert_eq!(funding_tx, inputs[0]);
+        assert_eq!(tx.0.version, 2);
+
+        let inputs = tx.0.input;
+        assert_eq!(inputs.len(), 1);
+
+        let outputs = tx.0.output;
+        assert_eq!(outputs.len(), 2);
+
+        let out_0 = &outputs[0];
+        let out_1 = &outputs[1];
+
+        assert_ne!(out_0.value, 0);
+        assert_eq!(out_1.value, 0);
     }
 
     #[test]
     fn test_anchoring_transaction_builder_funds() {
-        unimplemented!();
+        let funding_tx0: Transaction = Transaction::from_hex(
+            "02000000000101b651818fe3855d0d5d74de1cf72b56503c16f808519440e842b6\
+             dc2dd570c4930100000000feffffff02deaa7b0000000000160014923904449829\
+             cd865cdfb72abdba0806ce9e48911027000000000000220020e9bb049fdff8f8d3\
+             b33b7335978b1dbb268833a32a69906f9e500e4103151bef02483045022100ddc7\
+             eb1193529a8d0e48cf24f536d5fbb5de3b67d2f56c98190ea8585d58a156022075\
+             e33981f1a7d78ce2915402d4b9b38b8d5311e0aef2e3ccf9284d2ce602968d0121\
+             021d0478acd223fb9b2ad7485f06f12914a1b7effc78390a08c50bfe53b3b24815\
+             062c1400",
+        ).unwrap();
+        let funding_tx1: Transaction = Transaction::from_hex(
+            "020000000001018aa4065d472efc80d2a9f26bf0f77aabd5b8fcb45661de8a0161\
+             cbcc6b5fef9e0000000000feffffff0235837b00000000001600143e9fd2829e66\
+             868739ddbb8c397a3e35ae02a5151027000000000000220020e9bb049fdff8f8d3\
+             b33b7335978b1dbb268833a32a69906f9e500e4103151bef0247304402201d2f3c\
+             a3ec4c82071b825a44c5b8a7455e4e50caef07e988bbe46554846e445702205f1b\
+             066bf6d747c06b3721ac878104e434e977e0e321191a0c860f05fb3bb319012103\
+             b475c0164be599df74ea5d4b669fe1c439953e40eea2d4958d66698f26eeaa5f2a\
+             2c1400",
+        ).unwrap();
+        let funding_tx2: Transaction = Transaction::from_hex(
+            "0200000000010115c9acef986ba57a7fcf43c6cb60221b70af1da6d3ad6d1e2480\
+             e55bc80c559c00000000171600147881a57eadd9361c497e2b1671da4ed1c0ac1e\
+             44feffffff02a086010000000000220020e9bb049fdff8f8d3b33b7335978b1dbb\
+             268833a32a69906f9e500e4103151bef406df6000000000016001424ff8bab4afa\
+             feca816e4a8300e135045ce15f6b02483045022100f6b55f77ec53e339d150637a\
+             76de5436165c27ea415a8175f5fdff634bf91cd402204252dbd3af0ba8a7490912\
+             68491169dca4477515e2b3155de04ffacfa39f00d4012102ad0617b920ce3a7a48\
+             1a10222344a7b338e7a13e8e725eb44a3a53354a90f9e32a2c1400",
+        ).unwrap();
+
+        let keys = vec![
+            "038b782f94d19f34536a96e12e0bad99e6f82c838fa16a4234572f5f132d95ba29",
+            "020ae2216f42575c4196864eda0252c75c61273065f691b32be9a99cb2a3c9b4d1",
+            "02536d5e1464b961562da57207e4a46edb7dade9b92aa29712ca8309c8aba5be5b",
+        ].iter()
+            .map(|h| PublicKey::from_hex(h).unwrap().0.clone())
+            .collect::<Vec<_>>();
+
+        let redeem_script = RedeemScriptBuilder::with_public_keys(keys)
+            .to_script()
+            .unwrap();
+
+        let (tx, inputs) = BtcAnchoringTransactionBuilder::new(redeem_script)
+            .additional_funds(funding_tx0.clone())
+            .additional_funds(funding_tx1.clone())
+            .additional_funds(funding_tx2.clone())
+            .fee(1)
+            .payload(Height::zero(), funding_tx0.hash())   // useless payload
+            .create()
+            .unwrap();
+
+        assert_eq!(inputs.len(), 3);
+        let inputs = tx.0.input;
+        assert_eq!(inputs.len(), 3);
+
+        let outputs = tx.0.output;
+        assert_eq!(outputs.len(), 2);
+
+        let out_0 = &outputs[0];
+        let out_1 = &outputs[1];
+
+        assert_ne!(out_0.value, 0);
+        assert_eq!(out_1.value, 0);
     }
 
     #[test]
+    #[should_panic]
     fn test_anchoring_transaction_builder_incorrect_prev_tx() {
-        unimplemented!();
+        let funding_tx: Transaction = Transaction::from_hex(
+            "02000000000101b651818fe3855d0d5d74de1cf72b56503c16f808519440e842b6\
+             dc2dd570c4930100000000feffffff02deaa7b0000000000160014923904449829\
+             cd865cdfb72abdba0806ce9e48911027000000000000220020e9bb049fdff8f8d3\
+             b33b7335978b1dbb268833a32a69906f9e500e4103151bef02483045022100ddc7\
+             eb1193529a8d0e48cf24f536d5fbb5de3b67d2f56c98190ea8585d58a156022075\
+             e33981f1a7d78ce2915402d4b9b38b8d5311e0aef2e3ccf9284d2ce602968d0121\
+             021d0478acd223fb9b2ad7485f06f12914a1b7effc78390a08c50bfe53b3b24815\
+             062c1400",
+        ).unwrap();
+
+        let keys = vec![
+            "038b782f94d19f34536a96e12e0bad99e6f82c838fa16a4234572f5f132d95ba29",
+            "020ae2216f42575c4196864eda0252c75c61273065f691b32be9a99cb2a3c9b4d1",
+            "02536d5e1464b961562da57207e4a46edb7dade9b92aa29712ca8309c8aba5be5b",
+        ].iter()
+            .map(|h| PublicKey::from_hex(h).unwrap().0.clone())
+            .collect::<Vec<_>>();
+
+        let prev_tx: Transaction = Transaction::from_hex(
+            "01000000000101348ead2317da8c6ae12305af07e33b8c0320c9319f21007a704e44f32e7a75500000000\
+             000ffffffff0250ec0e00000000002200200f2602a87bbdb59fdf4babfffd568ef39a85cf2f08858c8847\
+             d70f27894b68840000000000000000326a3045584f4e554d0100085200000000000085f467f2bad583dbb\
+             08f84a47e817d8293fb8c70d033604f441f53a6cc092f180500473044022003334a325c7c382aca17c9c0\
+             790d3e2a48fbc99fcc34eb6f73ac4386fcca290602206508789e66f246fc496784df97b0b9e895ae93948\
+             cf6a3a1ca2603d02a462c050148304502210081cadbe7c21e9e030b8ed9e3d084595833351284ce26d79d\
+             ad889ffdab73bfc402205dd359f19b6871f3e21b9b9c2a57aabd2ce64a0631a136fe4028dabd96fa89a20\
+             147304402200dc7a83d7064f74e2e7a90fdfab6b41ab8663b8151ae4e313bf29ee9c7c9f38e022043ca73\
+             74050f1f3e321fe686f6858f94b8d8d130e73b61a74e6200f75452edf30169532103f0c44bc5cd2747ac3\
+             4380e84ac4e78fac684848d32198bac5985d116c74ae6862103d9d4eb85dad869dc54a65f77a7e81eea0e\
+             a5d81129928d6d5b6dcb7b57c8991b21033ea315ab975c6424740c305db3f07b62b1121e27d3052b9a30d\
+             b56a8b504713c53ae00000000",
+        ).unwrap();
+
+        let redeem_script = RedeemScriptBuilder::with_public_keys(keys)
+            .to_script()
+            .unwrap();
+
+        let _ = BtcAnchoringTransactionBuilder::new(redeem_script)
+            .prev_tx(prev_tx)
+            .additional_funds(funding_tx);
     }
 
     #[test]
+    #[should_panic]
     fn test_anchoring_transaction_builder_incorrect_funds() {
-        unimplemented!();
+        let funding_tx: Transaction = Transaction::from_hex(
+            "020000000001015315c18b6a6893ec08d4a7175da494d5d856a8efc983ba2e8eed06c2211041f500000000\
+             00feffffff028c5b7b0000000000160014d00d6944fb51eda26fb450809bd3388cf51462d4102700000000\
+             0000220020c622abf40381f842e9a860c2353405a0c73534093a6fe9c661fd487c5bc36d99024730440220\
+             5345da1affdc26eda5415c31a8fd9afd6280d42f411c43ec4e7a4f284d67da4102205039e7fe4a2cce1864\
+             91f88d319e6001a45078c95b384f293669f00793e1e94b0121022b2bc757808dd27b3490b372764ff61872\
+             1b11577d52837da3d0daac467df91c2f2c1400"
+        ).unwrap();
+
+        let keys = vec![
+            "038b782f94d19f34536a96e12e0bad99e6f82c838fa16a4234572f5f132d95ba29",
+            "020ae2216f42575c4196864eda0252c75c61273065f691b32be9a99cb2a3c9b4d1",
+            "02536d5e1464b961562da57207e4a46edb7dade9b92aa29712ca8309c8aba5be5b",
+        ].iter()
+            .map(|h| PublicKey::from_hex(h).unwrap().0.clone())
+            .collect::<Vec<_>>();
+
+        let redeem_script = RedeemScriptBuilder::with_public_keys(keys)
+            .to_script()
+            .unwrap();
+
+        let _ = BtcAnchoringTransactionBuilder::new(redeem_script).additional_funds(funding_tx);
     }
 }
