72ddaada7b4027d772361d6b2620231720544cb4
Alexander Ponimaskin <ponimaskin@gmail.com>
2018-06-14 17:58:32 +0300

    fix missing input signatures

--- src/handler.rs
+++ src/handler.rs
@@ -89,11 +89,17 @@ impl<'a> UpdateAnchoringChainTask<'a> {
 
         for (index, proposal_input) in proposal_inputs.iter().enumerate() {
             let input_id = TxInputId::new(proposal.id(), index as u32);
-            if anchoring_schema
-                .transaction_signatures()
-                .contains(&input_id)
+
+            if let Some(input_signatures) = anchoring_schema.transaction_signatures().get(&input_id)
             {
-                continue;
+                if input_signatures.contains(validator_id) {
+                    trace!(
+                        " {:?} is already signed by validator {}",
+                        input_id,
+                        validator_id
+                    );
+                    continue;
+                }
             }
 
             let signature = signer.sign_input(
