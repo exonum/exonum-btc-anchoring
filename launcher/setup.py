242fb46a73be942889e99488393284786ed64613
Aleksei Sidorov <aleksei.sidorov@xdev.re>
2019-10-16 13:12:36 +0300

    Tiny fixes

--- launcher/setup.py
+++ launcher/setup.py
@@ -6,7 +6,7 @@ install_requires = []  # Since `exonum-launcher` is not on pypi yet, suppose we 
 python_requires = ">=3.6"
 
 setup(
-    name="exonum_btc-anchoring_plugin",
+    name="exonum_btc_anchoring_plugin",
     version="0.1",
     description="Exonum BTC anchoring plugin",
     url="https://github.com/exonum/exonum-btc-anchoring",
